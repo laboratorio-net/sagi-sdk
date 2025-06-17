cbdccc13c72bd05f2422f66c2ed2e450751ad3b3
Leandro Alves <leandro.silva.alves86@gmail.com>
2025-06-17 20:00:07 -0300

    feat: Implement configuration loading for AWS settings from appsettings.json and update DynamoDbHosting initialization

--- Sagi.Sdk.AWS.DynamoDb/src/Samples/Program.cs
+++ Sagi.Sdk.AWS.DynamoDb/src/Samples/Program.cs
@@ -1,16 +1,24 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 using Sagi.Sdk.AWS.DynamoDb.Hosting;
 
 using Samples.Services;
 using Samples.Tables;
 
+var config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: true)
+    .AddEnvironmentVariables()
+    .Build();
+
+var options = config.GetSection("AWS").Get<AwsOptions>()!;
+
 await DynamoDbHosting
     .RunAsync(x =>
     {
-        x.Accesskey = "root";
-        x.SecretKey = "secret";
-        x.ServiceURL = "http://localhost:8000";
+        x.Accesskey = options.Accesskey;
+        x.SecretKey = options.SecretKey;
+        x.ServiceURL = options.ServiceUrl;
         x.InitializeDb = true;
         x.ConfigureTable(new OrderTable());
         x.ConfigureTable(new PaymentTable());
@@ -22,3 +30,5 @@ await DynamoDbHosting
 
 Console.CancelKeyPress += (obj, args)
     => DynamoDbHosting.StopAsync().Wait();
+
+record AwsOptions(string Accesskey, string SecretKey, string ServiceUrl);
